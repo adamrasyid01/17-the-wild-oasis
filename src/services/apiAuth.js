6a9e59bbd042446104e67dd2cbbe0dfad6950e23
Adam Rasyid N <adamrasyid01@gmail.com>
2026-07-07 17:00:20 +0700

    Functionality Logout, Update Password, Sigup

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -1,5 +1,21 @@
-import { supabase } from "./supabase";
+import { supabase, supabaseUrl } from "./supabase";
 
+export async function signup({ fullName, email, password }) {
+  const { data, error } = await supabase.auth.signUp({
+    email: email,
+    password: password,
+    options: {
+      data: {
+        fullName: fullName,
+        avatar: "",
+      },
+    },
+  });
+
+  if (error) throw new Error(error.message);
+
+  return data;
+}
 export async function Login({ email, password }) {
   const { data, error } = await supabase.auth.signInWithPassword({
     email,
@@ -10,4 +26,49 @@ export async function Login({ email, password }) {
 
   console.log(data);
   return data;
+}
+
+export async function getCurrentUser() {
+  const { data: session } = await supabase.auth.getSession();
+
+  if (!session.session) return null;
+
+  const { data, error } = await supabase.auth.getUser();
+
+  if (error) throw new Error(error.message);
+
+  return data?.user;
+}
+
+export async function logout() {
+  const { error } = await supabase.auth.signOut();
+
+  if (error) throw new Error(error.message);
+}
+
+export async function updateCurrentUser({ password, fullName, avatar }) {
+  // 1. Update password OR fullName
+  let updateData;
+
+  if (password) updateData = { password };
+  if (fullName) updateData = { data: { fullName } };
+  const { data, error } = await supabase.auth.updateUser(updateData);
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  // 2. Upload the avatar image
+  const fileName = `avatar-${data.user.id}-${Math.random()}`;
+
+  const { error: storageError } = await supabase.storage.from("avatars").upload(fileName, avatar);
+
+  if (storageError) throw new Error(storageError.message);
+  // 3. Update avatar
+  const { data: udpdatedUser, error: error2 } = supabase.auth.updateUser({
+    data: {
+      avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+    },
+  });
+
+  if (error2) throw new Error(error2.message);
+  return udpdatedUser;
 }
